5a76f6722c85289e8ce70a303a5696aafa97ccb0
Brenda309 <brewihogora23@gmail.com>
2022-08-16 12:09:02 +0200

    Update the data in table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-CREATE TABLE Animal ( id INT GENERATED ALWAYS AS IDENTITY, 
+CREATE TABLE Animals ( id INT GENERATED ALWAYS AS IDENTITY, 
 name varchar(110), 
 date_of_birth Date, 
 escape_attempts INT, 
