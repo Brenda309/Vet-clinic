1222c138a9f6d0e71269656628b767145d8fbcd9
Brenda309 <brewihogora23@gmail.com>
2022-08-16 12:34:38 +0200

    Fetch names of animals born between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM Animals WHERE name LIKE '%mon';
+SELECT * FROM Animals WHERE name LIKE '%mon'; 
+SELECT name FROM Animals WHERE date_of_birth BETWEEN 'Jan, 1, 2016' AND 'Dec, 31, 2019';
+
+
