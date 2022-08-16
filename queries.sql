6c60017386876f5db14b7aec0f7b6c3145a0097e
Brenda309 <brewihogora23@gmail.com>
2022-08-16 13:05:53 +0200

    Fetch more queries

--- queries.sql
+++ queries.sql
@@ -2,5 +2,13 @@
 
 SELECT * FROM Animals WHERE name LIKE '%mon'; 
 SELECT name FROM Animals WHERE date_of_birth BETWEEN 'Jan, 1, 2016' AND 'Dec, 31, 2019';
+SELECT name FROM Animals WHERE neutered= true AND escape_attempts < 3;
+SELECT date_of_birth FROM Animals WHERE name = 'Agumon' OR  name = 'Pikachu';
+SELECT name, escape_attempts FROM Animals WHERE weight_kg > 10.5;
+SELECT * FROM Animals WHERE neutered = True;
+
+
+
+
 
 
