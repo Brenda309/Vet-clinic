52ccb80d767d815a5895538a75af6e6b8210bd65
Brenda309 <brewihogora23@gmail.com>
2022-08-18 21:17:19 +0200

    return the number  of all animals.

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * FROM Animals WHERE name LIKE '%mon'; 
 SELECT name FROM Animals WHERE date_of_birth BETWEEN 'Jan, 1, 2016' AND 'Dec, 31, 2019';
-SELECT name FROM Animals WHERE neutered= true AND escape_attempts < 3;
+SELECT name FROM Animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM Animals WHERE name = 'Agumon' OR  name = 'Pikachu';
 SELECT name, escape_attempts FROM Animals WHERE weight_kg > 10.5;
 SELECT * FROM Animals WHERE neutered = True;
@@ -66,3 +66,23 @@ WHERE weight_kg < 0;
 
 /*How many animals are there*/
 SELECT COUNT(*) FROM Animals;
+
+/*How many animals have never tried to escape?*/
+
+SELECT COUNT(*) FROM Animals
+WHERE escape_attempts <= 0;
+
+/*What is the average weight of animals?*/
+
+SELECT AVG(weight_kg) FROM Animals;
+/* who escape the most */
+SELECT neutered, SUM(escape_attempts) FROM Animals GROUP BY neutered;
+
+/*Min and max weight each animal species */
+
+ SELECT species, MIN(weight_kg), Max(weight_kg) FROM Animals GROUP BY species;
+
+/*AVG number of escape attempt for each animal species born  between 1990 and 2000*/
+SELECT species, AVG(escape_attempts) FROM Animals 
+ WHERE date_of_birth BETWEEN 'Jan, 1, 1990' AND 'Dec, 31, 2000'
+ GROUP BY species;
