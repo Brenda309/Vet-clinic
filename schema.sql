0344b337b712702a9b2a3aa84e4639954e1f4082
Brenda309 <brewihogora23@gmail.com>
2022-08-22 18:58:23 +0200

    Delete all previous code

--- schema.sql
+++ schema.sql
@@ -47,39 +47,6 @@ CREATE TABLE Owners (
  id SERIAL PRIMARY KEY,
  name varchar(111));
 
- /*vets*/
-CREATE TABLE Vets (
- id SERIAL PRIMARY KEY,
- name varchar(111),
- age INT,
- date_of_graduation Date);
-
-/*Joining table*/
-CREATE TABLE specializations(
-  vet_id INT ,
-  spicies_id INT ,
-  PRIMARY KEY(vet_id, spicies_id));
-
-/*Visitis*/
-CREATE TABLE Visits(
-  vet_id INT,
-  animal_id INT,
-  date_of_visit date,
-  PRIMARY KEY(vet_id, animal_id, date_of_visit));
+ 
 
 
-ALTER TABLE Visits 
-ADD FOREIGN KEY(animal_id) 
-REFERENCES Animals(id);
-
-ALTER TABLE Visits 
-ADD FOREIGN KEY(vet_id) 
-REFERENCES Vets(id);
-
-ALTER TABLE specializations 
-ADD FOREIGN KEY(spicies_id) 
-REFERENCES Species(id);
-
-ALTER TABLE specializations 
-ADD FOREIGN KEY(vet_id)
- REFERENCES Vets(id);
