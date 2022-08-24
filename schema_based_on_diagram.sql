65cf856b21719884386d8ec1b1ef31096237d02d
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-08-24 20:04:10 +0300

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,3 +2,11 @@ CREATE TABLE patients (
  id SERIAL PRIMARY KEY,
  name varchar(111),
  date_of_birth date);
+
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at timestamp NOT NULL,
+  patient_id int NOT NULL,
+  status varchar(50) NOT NULL,
+  FOREIGN KEY (patient_id) REFERENCES patients (id)
+);
